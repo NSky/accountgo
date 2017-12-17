69a164a1edfc4fe82f6525572d238fc4389f978a
Eugene Lipchansky <eugene.lipchansky@gmail.com>
2017-12-18 00:53:00 +0300

    Revert "remove exact Nlog folder and Web"
    This reverts commit 10189f02ab33b4d069d58eb0403e595f54109986.

---
+++ src/Web/ModelsApi/Purchasing/VendorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.ModelsApi.Purchasing
+{
+    public class VendorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int PaymentTermId { get; set; }
+    }
+}
